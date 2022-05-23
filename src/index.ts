3e8a3f13c7aab295a3d61a3acbe3f4cace0c226e
ironPriest <iron.priest.mih@gmail.com>
2022-05-23 15:00:56 +0300

    first

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import express, {Request, Response} from 'express'
+import cors from 'cors'
+import bodyParser from "body-parser";
+
+// create express app
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors())
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
+
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+const adresses = [{id: 1, value: 'Nezalezhnasti 12'}, {id: 2, value: 'Selickaga 11'}]
+
+app.get('/products', (req: Request, res: Response) => {
+    if (req.query.title) {
+        let searchString = req.query.title.toString()
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1))
+    } else {
+        res.send(products)
+    }
+})
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(p => p.id === +req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
+app.put('/products/:id', (req: Request, res: Response) => {
+    let product = products.find(p => p.id === +req.params.id)
+    if (product) {
+        product.title = req.body.title
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
+app.delete('/products/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1);
+            res.send(204)
+            return;
+        }
+    }
+    res.send(404)
+})
+
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct = {
+        id: +(new Date()),      // id generation
+        title: req.body.title   // getting title from req body
+    }
+    products.push(newProduct)
+
+    res.status(201).send(newProduct)
+})
+
+app.get('/adresses', (req: Request, res: Response) => {
+    res.send(adresses)
+})
+
+app.get('/adresses/:id', (req: Request, res: Response) => {
+    let adress = adresses.find(p => p.id === +req.params.id)
+    if (adress) {
+        res.send(adress)
+    } else {
+        res.send(404)
+    }
+})
+
+// start app
+app.listen(port, () => {
+    console.log('Example app listening on port: ${port}')
+})
